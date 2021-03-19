8f59cf1f9b50f48658e889ec8cdf6013f7b11d7d
SpanishPear <shrey.somaiya@gmail.com>
2021-03-19 22:36:39 +1100

    hello world

---
+++ hello_world/main.rs
@@ -0,0 +1,12 @@
+
+fn main() {
+
+	// this is a Rust macro
+	// Q: not a function??
+	// A: vaargs are not in rust
+
+	// Q: why?
+	// A: C style varrargs are unsave, C++ uses templates, probably more reasons.
+	//    something something safety
+    println!("Hello, world!");
+}
