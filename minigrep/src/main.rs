8cf097a7fe4b78d99653611edaeac7c7f47fe0fd
SpanishPear <shrey.somaiya@gmail.com>
2021-05-13 00:51:16 +1000

    update erroors handling + new method on config fix

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,13 +1,17 @@
 use std::env;
 use std::fs;
+use std::process;
 
 fn main() {
     // get env::args (a collection) and collect into an iterable of strings
     let args: Vec<String> = env::args().collect();
 
     // cla's start at 0 with program name
-    let config: Config = Config::new(&args);
-
+    let config: Config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+    
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
 
@@ -24,11 +28,16 @@ struct Config {
 
 impl Config {
     // why is this String and not str
-    fn new(args: &[String]) -> Config {
+    fn new(args: &[String]) -> Result<Config, &str> {
+        
+        if args.len() < 3 {
+            return Err("Not enough Arguments")
+        }
+
         //let query = &args[1];
         //let filename = &args[2];
         // to copy strings into struct, we use clone (a la strcpy)
         // means that config now owns the string
-        Config { query: args[1].clone(), filename: args[2].clone() }
+        Ok (Config { query: args[1].clone(), filename: args[2].clone() })
     }
 }
