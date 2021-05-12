8c1fab0491965707f6efa7edc460333a984199d4
SpanishPear <shrey.somaiya@gmail.com>
2021-05-12 23:59:07 +1000

    read args and file

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,3 +1,18 @@
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    // get env::args (a collection) and collect into an iterable of strings
+    let args: Vec<String> = env::args().collect();
+
+    // cla's start at 0 with program name
+    let query = &args[1];
+    let filename = &args[2];
+
+    println!("Searching for {}", query);
+    println!("In file {}", filename);
+
+    let contents = fs::read_to_string(filename).expect("Something went wrong reading the file!");
+
+    println!("With text:\n{}", contents);
 }
